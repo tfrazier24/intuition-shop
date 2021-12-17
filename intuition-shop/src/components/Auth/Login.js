9eef339a91578be09e41451c536dea71ae020609
John Douglas Price <johndouglasprice@Johns-MacBook-Pro.local>
2021-12-17 12:45:38 -0500

    Finished login/register functionality and basic email authorization. If the user enters an email to login that does not exist a modal pops up that notifies them that the email is not recognized.

--- intuition-shop/src/components/Auth/Login.js
+++ intuition-shop/src/components/Auth/Login.js
@@ -1,15 +1,69 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import ISLogo from "../../assets/images/ISLogo.png"
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
+
+    const email = useRef();
+    const password = useRef();
+    const existDialog = useRef();
+    const navigate = useNavigate();
+    const [ modalIsOpen, setModalIsOpen ] = useState(false);
+
+    const existingUserCheck = () => {
+        return fetch(`http://localhost:8000/users?email=${email.current.value}`)
+        .then(res => res.json())
+        .then(user => user.length ? user[0] : false)
+    }
+
+    const setModalIsOpenToTrue = () => {
+        setModalIsOpen(true);
+    }
+    
+    const setModalIsOpenToFalse = () => {
+        setModalIsOpen(false);
+    }
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+
+        existingUserCheck()
+        .then(exists => {
+            if (exists) {
+                localStorage.setItem("user", exists.id)
+                navigate('/');
+            } else {
+                setModalIsOpenToTrue();
+            }
+        })
+    }
+
     return (
         <>
+            {
+            modalIsOpen ? 
+                <div class="modal is-active" id="modal">
+                    <div class="modal-background"></div>
+                    <div class="modal-card">
+                        <header class="modal-card-head">
+                            <p class="modal-card-title">Uh Oh! We've encountered a problem.</p>
+                            <button class="delete" aria-label="close" onClick={setModalIsOpenToFalse}></button>
+                        </header>
+                        <section class="modal-card-body">We don't recognize that email. Try again or register a new email.</section>
+                        <section class="modal-card-body"><a onClick={() => navigate('/register')}>Click here to register a new user</a></section>
+                        <footer class="modal-card-foot">
+                            <button class="button is-danger mr-auto" aria-label="close" onClick={setModalIsOpenToFalse}>Close</button>
+                        </footer>
+                    </div>
+                </div> : null
+            }
+
             <div class="columns is-centered">
                 <div class="column is-one-third mt-6 has-text-centered">
                     <div class="card p-6">
                         <div class="tabs is-centered is-boxed is-medium">
                             <li class="is-active"><a>Login</a></li>
-                            <li><a>Register</a></li>
+                            <li><a onClick={() => navigate("/register")}>Register</a></li>
                         </div>
                         <div class="field">
                             <div class="card-image">
@@ -19,7 +73,7 @@ export const Login = () => {
                             </div>
         
                             <p class="control has-icons-left has-icons-right">
-                                <input class="input" type="email" placeholder="Email" />
+                                <input class="input" type="email" placeholder="Email" ref={email} />
                                 <span class="icon is-small is-left">
                                 <i class="fas fa-envelope"></i>
                                 </span>
@@ -30,7 +84,7 @@ export const Login = () => {
                             </div>
                             <div class="field">
                             <p class="control has-icons-left">
-                                <input class="input" type="password" placeholder="Password" />
+                                <input class="input" type="password" placeholder="Password" ref={password} />
                                 <span class="icon is-small is-left">
                                 <i class="fas fa-lock"></i>
                                 </span>
@@ -38,7 +92,7 @@ export const Login = () => {
                             </div>
                             <div class="field">
                             <p class="control has-text-centered">
-                                <button class="button is-link mt-5">
+                                <button class="button is-link mt-5" onClick={handleLogin}>
                                 Login
                                 </button>
                             </p>
