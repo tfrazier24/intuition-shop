e8619a3d59354d9f31a0d427869714ed36d98b6a
John Douglas Price <johndouglasprice@Johns-MacBook-Pro.local>
2022-01-26 10:12:19 -0500

    Basic navbar finished

--- intuition-shop/src/App.js
+++ intuition-shop/src/App.js
@@ -3,10 +3,11 @@ import 'bulma/css/bulma.min.css';
 import { Route, Routes } from "react-router-dom";
 import { ApplicationViews } from './ApplicationViews';
 import { BrowserRouter as Router } from "react-router-dom";
+import { Navbar } from './components/nav/Navbar';
 
 
 export const App = () => (
-  <Router >
+  <Router>
     <ApplicationViews />
   </ Router>
 );
--- intuition-shop/src/components/HomePage.js
+++ intuition-shop/src/components/HomePage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Navbar } from "./nav/Navbar";
 
 export const HomePage = () => {
     
     return (
         <>
+        <Navbar />
          <p>Turtles</p>
         </>
     );
