df93930b0593150726f93d2b9deefb419dfac34f
John Douglas Price <johndouglasprice@Johns-MacBook-Pro.local>
2022-01-29 12:38:36 -0500

    Product page. Design not finished yet.

--- intuition-shop/src/App.js
+++ intuition-shop/src/App.js
@@ -4,11 +4,14 @@ import { Route, Routes } from "react-router-dom";
 import { ApplicationViews } from './ApplicationViews';
 import { BrowserRouter as Router } from "react-router-dom";
 import { Navbar } from './components/nav/Navbar';
+import { ProductProvider } from './providers/ProductProvider';
 
 
 export const App = () => (
   <Router>
-    <ApplicationViews />
+    <ProductProvider>
+      <ApplicationViews />
+    </ProductProvider>
   </ Router>
 );
 
--- intuition-shop/src/ApplicationViews.js
+++ intuition-shop/src/ApplicationViews.js
@@ -3,13 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./components/HomePage";
 import { Login } from "./components/auth/Login";
 import { Register } from "./components/auth/Register";
+import { ProductList } from "./components/products/ProductList";
+import { ProductProvider } from "./providers/ProductProvider";
 
 export const ApplicationViews = () => {
     return (
         <main>
             <Routes>
                 <Route exact path="/" element={<HomePage />} /> 
+
+            
+                    <Route exact path="/products" element={<ProductList />} />
                 
+
                 <Route exact path="/login" element={<Login />} />
 
                 <Route exact path="/register" element={<Register />} />
